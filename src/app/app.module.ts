8725f17ae5089e70cb3f86b6201e3ad27ec7f358
Daniel Foltut <danfoltut@gmail.com>
2022-08-21 12:21:16 +0300

    Project structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './content/pages/home/home.component';
+import { GlobalNavComponent } from './content/components/global-nav/global-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GlobalNavComponent
   ],
   imports: [
     BrowserModule,
